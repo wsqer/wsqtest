25ecd405d9326c9ffa52c7e5ef9d03406ed85fa1
wsqer <15182615610@163.com>
2017-07-11 13:14:35 +0800

    test update

--- test.cpp
+++ test.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
+class Test
+{
+	private:
+		int num;
+	public:
+		int getnum()
+		{
+			return num;
+		}
+		Test(int n)
+		{
+			num=n;
+		}
+		~Test()
+		{
+			cout<<"Test died"<<endl;OB
+		}
+};
 int main()
 {
    cout<<"test"<<endl;
+   Test t1(12);
+   cout<<t1.getnum()<<endl;
+   
+
 }
